4a2ec26473ebfa4f3733e61e3e0591f2b7c450b7
nullnull-kim <nullnull.kim@gmail.com>
2023-01-21 15:00:55 +0900

    feat: 5.2.1

--- src/main/java/model/HttpRequest.java
+++ src/main/java/model/HttpRequest.java
@@ -3,6 +3,7 @@ package model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 import java.io.*;
 import java.util.Arrays;
@@ -21,33 +22,67 @@ public class HttpRequest {
     private Function<String, String[]> function2 = (queries) -> queries.split("=");
     public HttpRequest(InputStream in) {
         try {
-            BufferedReader bf = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String firstLine = bf.readLine();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String firstLine = br.readLine();
             if (firstLine == null) return;
 
-            String[] split = firstLine.split(" ");
-            if (split.length != 3) return;
+            processRequestLine(firstLine);
 
-            this.method = split[0];
-
-            String line = bf.readLine();
-            while (!"".equals(line)) {
-                if (null == line || "".equals(line)) break;
-                String[] headers = line.split(":");
-                this.header.put(headers[0].trim(), headers[1].trim());
-                line = bf.readLine();
+            String line = br.readLine();
+            while (null != line && !line.equals("")) {
+                logger.debug("header : {}", line);
+                String[] tokens = line.split(":");
+                header.put(tokens[0].trim(), tokens[1].trim());
+                line = br.readLine();
             }
 
-            if ("GET".equals(method)) {
-                setPathAndParameter(split[1]);
-            } else if ("POST".equals(method)) {
-                setParameter(bf.readLine());
-                this.path = split[1];
+            if ("POST".equals(method)) {
+                String body = IOUtils.readData(br, Integer.parseInt(header.get("Content-Length")));
+                parameter = HttpRequestUtils.parseQueryString(body);
             }
+
+//            String[] split = firstLine.split(" ");
+//            if (split.length != 3) return;
+//
+//            this.method = split[0];
+//
+//            String line = bf.readLine();
+//            while (!"".equals(line)) {
+//                if (null == line || "".equals(line)) break;
+//                String[] headers = line.split(":");
+//                this.header.put(headers[0].trim(), headers[1].trim());
+//                line = bf.readLine();
+//            }
+//
+//            if ("GET".equals(method)) {
+//                setPathAndParameter(split[1]);
+//            } else if ("POST".equals(method)) {
+//                setParameter(bf.readLine());
+//                this.path = split[1];
+//            }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
 
+    }
+
+    private void processRequestLine(String firstLine) {
+        logger.debug("request line : {}", firstLine);
+        String[] tokens = firstLine.split(" ");
+        method = tokens[0];
+
+        if ("POST".equals(method)) {
+            path = tokens[1];
+            return;
+        }
+
+        int index = tokens[1].indexOf("?");
+        if (index == -1) {
+            path = tokens[1];
+        } else {
+            path = tokens[1].substring(0, index);
+            parameter = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
+        }
     }
 
     private void setPathAndParameter(String path) {
