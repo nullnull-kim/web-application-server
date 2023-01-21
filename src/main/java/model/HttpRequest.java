4e770a2228f2e6e814cf6bcaaa323b8aebcce55d
nullnull-kim <nullnull.kim@gmail.com>
2023-01-21 15:30:52 +0900

    feat: RequestLine을 이용한 HttpRequest Refactoring

--- src/main/java/model/HttpRequest.java
+++ src/main/java/model/HttpRequest.java
@@ -16,17 +16,18 @@ public class HttpRequest {
     private final static Logger logger = LoggerFactory.getLogger(HttpRequest.class);
     private String method;
     private String path;
+    private RequestLine requestLine;
     private Map<String, String> header = new HashMap<>();
     private Map<String, String> parameter = new HashMap<>();
-    private Function<String, String[]> function1 = (queries) -> queries.split("&");
-    private Function<String, String[]> function2 = (queries) -> queries.split("=");
+//    private Function<String, String[]> function1 = (queries) -> queries.split("&");
+//    private Function<String, String[]> function2 = (queries) -> queries.split("=");
     public HttpRequest(InputStream in) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String firstLine = br.readLine();
             if (firstLine == null) return;
 
-            processRequestLine(firstLine);
+            requestLine = new RequestLine(firstLine);
 
             String line = br.readLine();
             while (null != line && !line.equals("")) {
@@ -36,9 +37,11 @@ public class HttpRequest {
                 line = br.readLine();
             }
 
-            if ("POST".equals(method)) {
+            if ("POST".equals(getMethod())) {
                 String body = IOUtils.readData(br, Integer.parseInt(header.get("Content-Length")));
                 parameter = HttpRequestUtils.parseQueryString(body);
+            } else {
+                parameter = requestLine.getParams();
             }
 
 //            String[] split = firstLine.split(" ");
@@ -66,49 +69,51 @@ public class HttpRequest {
 
     }
 
-    private void processRequestLine(String firstLine) {
-        logger.debug("request line : {}", firstLine);
-        String[] tokens = firstLine.split(" ");
-        method = tokens[0];
+//    private void processRequestLine(String firstLine) {
+//        logger.debug("request line : {}", firstLine);
+//        String[] tokens = firstLine.split(" ");
+//        method = tokens[0];
+//
+//        if ("POST".equals(method)) {
+//            path = tokens[1];
+//            return;
+//        }
+//
+//        int index = tokens[1].indexOf("?");
+//        if (index == -1) {
+//            path = tokens[1];
+//        } else {
+//            path = tokens[1].substring(0, index);
+//            parameter = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
+//        }
+//    }
 
-        if ("POST".equals(method)) {
-            path = tokens[1];
-            return;
-        }
-
-        int index = tokens[1].indexOf("?");
-        if (index == -1) {
-            path = tokens[1];
-        } else {
-            path = tokens[1].substring(0, index);
-            parameter = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
-        }
-    }
-
-    private void setPathAndParameter(String path) {
-        if(path.contains("?")) {
-            String[] pathAndQuery = path.split("[?]");
-            this.path = pathAndQuery[0];
-            Arrays.asList(function1.apply(pathAndQuery[1])).stream()
-                    .map(s -> function2.apply(s))
-                    .forEach(query -> this.parameter.put(query[0], query[1]));
-        }
-        else this.path = path;
-    }
-
-    private void setParameter(String body) {
-        if(body == null) return;
-        Arrays.asList(function1.apply(body)).stream()
-                .map(s -> function2.apply(s))
-                .forEach(query -> this.parameter.put(query[0], query[1]));
-    }
+//    private void setPathAndParameter(String path) {
+//        if(path.contains("?")) {
+//            String[] pathAndQuery = path.split("[?]");
+//            this.path = pathAndQuery[0];
+//            Arrays.asList(function1.apply(pathAndQuery[1])).stream()
+//                    .map(s -> function2.apply(s))
+//                    .forEach(query -> this.parameter.put(query[0], query[1]));
+//        }
+//        else this.path = path;
+//    }
+//
+//    private void setParameter(String body) {
+//        if(body == null) return;
+//        Arrays.asList(function1.apply(body)).stream()
+//                .map(s -> function2.apply(s))
+//                .forEach(query -> this.parameter.put(query[0], query[1]));
+//    }
 
     public String getMethod() {
-        return method;
+//        return method;
+        return requestLine.getMethod();
     }
 
     public String getPath() {
-        return path;
+//        return path;
+        return requestLine.getPath();
     }
 
     public Map<String, String> getHeader() {
