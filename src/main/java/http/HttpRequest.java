892b2e4018cb6de06aa7148022b8d7ffe1f29c4f
nullnull-kim <nullnull.kim@gmail.com>
2023-01-23 22:41:01 +0900

    6.3.2 쿠키를 활용해 아이디 전달

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -17,8 +17,6 @@ public class HttpRequest {
     private RequestLine requestLine;
     private Map<String, String> header = new HashMap<>();
     private Map<String, String> parameter = new HashMap<>();
-//    private Function<String, String[]> function1 = (queries) -> queries.split("&");
-//    private Function<String, String[]> function2 = (queries) -> queries.split("=");
     public HttpRequest(InputStream in) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
@@ -41,69 +39,11 @@ public class HttpRequest {
             } else {
                 parameter = requestLine.getParams();
             }
-
-//            String[] split = firstLine.split(" ");
-//            if (split.length != 3) return;
-//
-//            this.method = split[0];
-//
-//            String line = bf.readLine();
-//            while (!"".equals(line)) {
-//                if (null == line || "".equals(line)) break;
-//                String[] headers = line.split(":");
-//                this.header.put(headers[0].trim(), headers[1].trim());
-//                line = bf.readLine();
-//            }
-//
-//            if ("GET".equals(method)) {
-//                setPathAndParameter(split[1]);
-//            } else if ("POST".equals(method)) {
-//                setParameter(bf.readLine());
-//                this.path = split[1];
-//            }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
 
     }
-
-//    private void processRequestLine(String firstLine) {
-//        logger.debug("request line : {}", firstLine);
-//        String[] tokens = firstLine.split(" ");
-//        method = tokens[0];
-//
-//        if ("POST".equals(method)) {
-//            path = tokens[1];
-//            return;
-//        }
-//
-//        int index = tokens[1].indexOf("?");
-//        if (index == -1) {
-//            path = tokens[1];
-//        } else {
-//            path = tokens[1].substring(0, index);
-//            parameter = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
-//        }
-//    }
-
-//    private void setPathAndParameter(String path) {
-//        if(path.contains("?")) {
-//            String[] pathAndQuery = path.split("[?]");
-//            this.path = pathAndQuery[0];
-//            Arrays.asList(function1.apply(pathAndQuery[1])).stream()
-//                    .map(s -> function2.apply(s))
-//                    .forEach(query -> this.parameter.put(query[0], query[1]));
-//        }
-//        else this.path = path;
-//    }
-//
-//    private void setParameter(String body) {
-//        if(body == null) return;
-//        Arrays.asList(function1.apply(body)).stream()
-//                .map(s -> function2.apply(s))
-//                .forEach(query -> this.parameter.put(query[0], query[1]));
-//    }
-
     public HttpMethod getMethod() {
 //        return method;
         return requestLine.getMethod();
@@ -128,5 +68,9 @@ public class HttpRequest {
 
     public String getParameter(String name) {
         return parameter.get(name);
+    }
+
+    public HttpCookie getCookies() {
+        return new HttpCookie(getHeader("Cookie"));
     }
 }
